c48f981de69754a301e9e48e8adcba842c6fc85f
chanx <chanxuhui@outlook.com>
2020-08-25 12:56:33 +0800

    Add Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
 public class Duke {
     public static void main(String[] args) {
+        printIntroMsg();
+
+        printByeMsg();
+    }
+
+    private static void printIntroMsg(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printHorizontalLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printEmptyLine();
+        printHorizontalLine();
+    }
+
+    private static void printByeMsg(){
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printEmptyLine();
+        printHorizontalLine();
+    }
+
+    private static void printHorizontalLine(){
+        System.out.println("-----------------------------------------------");
+    }
+
+    private static void printEmptyLine(){
+        System.out.println();
     }
 }
